a9b730d1b911e11d48fe12cdbafb53e4d9bb18a5
Simon Meersschaut <simon.meersschaut@gmail.com>
2026-04-01 13:42:04 +0200

    Added plot

---
+++ opdracht2/main.m
@@ -0,0 +1,12 @@
+f = @(x) x.^3  - 10;
+% f = @(x) - cos(abs(x) + pi/4) + 0.8;
+% f = @(x) cosh(x - exp(1)) - 1;
+% f = @(x) (8.*(x.^3) - 12*(x.^2) + 6.*x - 1)*exp(x);
+
+
+derf = @(x) x.^2 + 1;
+tol = 1e-2;
+maxiter = 100;
+x0 = 1;
+
+conv_fout_fig(f, derf, tol, maxiter, x0);
